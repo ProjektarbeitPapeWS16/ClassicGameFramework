5b1f2c4c85856b4a02e602fd4ef12b4b9d6f45ef
dojo1017 <dojo17@hs-karlsruhe.de>
2017-02-17 16:12:18 +0100

    Add Diagram

---
+++ SpacePanic/Stage1.cpp
@@ -0,0 +1,64 @@
+﻿#include "Stage1.h"
+
+void Stage1::setPlayer(PlayerEntity* player)
+{
+	delete this->player;
+	this->player = player;
+}
+
+void Stage1::addEnemy(EnemyEntity* enemy) const
+{
+	enemys->push_back(enemy);
+}
+
+void Stage1::addBackgroundEntity(Entity* backgroundEntity) const
+{
+	backgroundEntities->push_back(backgroundEntity);
+}
+
+Stage1::Stage1(GameConfig* config): Level(config->getRasterColumnsCount(), config->getRasterRowsCount(), config->getRasterWidth(), config->getRasterHeight(), FILE_PATH),
+                                    gameConfig(config),
+                                    FILE_PATH("levels/stage1.txt"),
+                                    enemys(new std::vector<EnemyEntity*>()),
+                                    backgroundEntities(new std::vector<Entity*>()),
+                                    allEntities(new std::vector<Entity*>()),
+                                    physicalObjects(new std::vector<PhysicalObject*>())
+{
+
+}
+
+
+
+const char* Stage1::getFilePath() const
+{
+	return FILE_PATH;
+}
+
+PlayerEntity* Stage1::getPlayer() const
+{
+	return player;
+}
+
+std::vector<EnemyEntity*>* Stage1::getEnemys() const
+{
+	return enemys;
+}
+
+std::vector<Entity*>* Stage1::getBackgroundEntities() const
+{
+	return backgroundEntities;
+}
+
+Stage1::~Stage1()
+{
+}
+
+std::vector<Entity*>* Stage1::getEntities() const
+{
+	return allEntities;
+}
+
+std::vector<PhysicalObject*>* Stage1::getPhysicalObjects()
+{
+	return physicalObjects;
+}
---
+++ SpacePanic/Stage1.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "Level.h"
+#include "GameConfig.h"
+#include "Entity.h"
+#include "PlayerEntity.h"
+#include "EnemyEntity.h"
+
+class Stage1 : public Level
+{
+	GameConfig* gameConfig;
+	const char* FILE_PATH;// = new std::string("levels/stage1.txt");
+	PlayerEntity* player;
+	std::vector<EnemyEntity*>* enemys;
+	std::vector<Entity*>* backgroundEntities;
+	std::vector<Entity*>* allEntities;
+	std::vector<PhysicalObject*>* physicalObjects;
+
+public:
+	void setPlayer(PlayerEntity* player);
+	void addEnemy(EnemyEntity* enemy) const;
+	void addBackgroundEntity(Entity* backgroundEntity) const;
+	Stage1(GameConfig* config);
+	const char* getFilePath() const;
+	PlayerEntity* getPlayer() const;
+	std::vector<EnemyEntity*>* getEnemys() const;
+	std::vector<Entity*>* getBackgroundEntities() const;
+	~Stage1() override;
+	std::vector<PhysicalObject*>* getPhysicalObjects() override;
+	std::vector<Entity*>* getEntities() const;
+};
